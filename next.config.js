df6ea148c5b6917fc92ae884fa76f05a19fbb8da
C.J. Stryker <chrisjstryker@gmail.com>
2023-03-18 10:30:29 +0000

    Finished optimization of Chat GPT-4 UI code.

--- next.config.js
+++ next.config.js
@@ -3,18 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  distDir: 'build',
-  nextConfig,
-  nextConfigFile: 'next.config.js',
-  webpack: (config, { isServer }) => {
-    // Modify the webpack configuration
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      };
-    }
-
-    return nextConfig;
-  },
-};
+module.exports = nextConfig
